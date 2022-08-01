a34452d765389eb6fe4bb6af4e63463b72e5a79b
SANAYEE <dudtkd0219@gmail.com>
2022-08-01 13:23:28 +0900

    7.2.2 요구사항 리팩토링 진행중

--- src/main/java/next/template/jdbc/InsertJdbcTemplate.java
+++ src/main/java/next/template/jdbc/InsertJdbcTemplate.java
@@ -1,10 +1,13 @@
 package next.template.jdbc;
 
+import java.sql.PreparedStatement;
+
 import next.dao.UserDao;
 import next.model.User;
 
 public class InsertJdbcTemplate {
 	public void insert(User user, UserDao dao) throws Exception {
-		dao.insert(user);
+		PreparedStatement pstmt = null;
+		dao.setValuesForInsert(user, pstmt);
 	}
 }
--- src/main/java/next/template/jdbc/UpdateJdbcTemplate.java
+++ src/main/java/next/template/jdbc/UpdateJdbcTemplate.java
@@ -1,10 +1,13 @@
-package next.template.jdbc;
+ package next.template.jdbc;
+
+import java.sql.PreparedStatement;
 
 import next.dao.UserDao;
 import next.model.User;
 
 public class UpdateJdbcTemplate {
 	public void update(User user, UserDao dao) throws Exception{
-		dao.update(user);
+		PreparedStatement pstmt = null;
+		dao.setValuesForUpdate(user, pstmt);
 	}
 }
