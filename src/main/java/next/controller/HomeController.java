df868073ef65fc5444e34b49cd45ca0d67a73e96
SANAYEE <dudtkd0219@gmail.com>
2022-08-03 11:10:29 +0900

    모든 질문 다 나오게 변경

--- src/main/java/next/controller/HomeController.java
+++ src/main/java/next/controller/HomeController.java
@@ -3,14 +3,26 @@ package next.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import core.mvc.Controller;
+import next.dao.QuestionsDao;
 import next.dao.UserDao;
 
 public class HomeController implements Controller {
+	
+	
+	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+	
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
         UserDao userDao = new UserDao();
         req.setAttribute("users", userDao.findAll());
+        
+        QuestionsDao qdao = new QuestionsDao();
+        req.setAttribute("questions", qdao.findAll());
+        log.debug("가져온 모든 질문글 : {}", qdao.findAll());
         return "home.jsp";
     }
 }
