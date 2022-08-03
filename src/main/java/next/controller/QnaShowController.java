3f0025ef5b4948ad84b7206c36ea832e96d4fdf8
SANAYEE <dudtkd0219@gmail.com>
2022-08-03 15:42:25 +0900

    상세 질문 화면 컨트롤러

--- src/main/java/next/controller/QnaShowController.java
+++ src/main/java/next/controller/QnaShowController.java
@@ -1,5 +1,7 @@
 package next.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,7 +9,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import core.mvc.Controller;
+import next.dao.AnswersDao;
 import next.dao.QuestionsDao;
+import next.model.Answers;
 import next.model.Questions;
 
 public class QnaShowController implements Controller {
@@ -23,11 +27,19 @@ public class QnaShowController implements Controller {
         log.debug("제대로 받아와요? {}", questionsId);
         QuestionsDao qdao = new QuestionsDao();
         Questions questions = qdao.findByQuestionsId(questionsId);
+        AnswersDao answerDao = new AnswersDao();
+        List<Answers> answers = answerDao.findByQuestionsId(questionsId);
         if(questions == null)
         	throw new NullPointerException("등록된 글이 아닙니다.");
+        if(answers == null) {
+        	log.debug("등록된 답변이 없어요..");
+        }
+        	
         
         req.setAttribute("questions", questions);
+        req.setAttribute("answers", answers);
         log.debug("제대로 받아와요? {}", questions);
+        log.debug("답변 제대로 받아와요? {}", answers);
         return "/qna/show.jsp";
     }
 
